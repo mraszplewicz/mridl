7caab200398ac2d01560250b59dafcea090109de
Maciej Raszplewicz <maciej.raszplewicz@gmail.com>
2015-03-08 13:35:48 +0100

    Documentation highlighting

---
+++ pl.mrasoft.mridl.ui/src/pl/mrasoft/mridl/ui/highlighting/MridlHighlightingConfiguration.java
@@ -0,0 +1,26 @@
+package pl.mrasoft.mridl.ui.highlighting;
+
+import org.eclipse.swt.graphics.RGB;
+import org.eclipse.xtext.ui.editor.syntaxcoloring.DefaultHighlightingConfiguration;
+import org.eclipse.xtext.ui.editor.syntaxcoloring.IHighlightingConfigurationAcceptor;
+import org.eclipse.xtext.ui.editor.utils.TextStyle;
+
+public class MridlHighlightingConfiguration extends
+		DefaultHighlightingConfiguration {
+
+	public static final String DOCUMENTATION_ID = "documentation";
+
+	@Override
+	public void configure(IHighlightingConfigurationAcceptor acceptor) {
+		super.configure(acceptor);
+		acceptor.acceptDefaultHighlighting(DOCUMENTATION_ID, "Documentation",
+				documentationTextStyle());
+	}
+
+	public TextStyle documentationTextStyle() {
+		TextStyle textStyle = defaultTextStyle().copy();
+		textStyle.setColor(new RGB(90, 180, 90));
+		return textStyle;
+	}
+
+}
